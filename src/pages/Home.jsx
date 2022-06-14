5db86c26fb828bb11d1aa1a394abef4ea475b755
eccotw8952 <pajiaduh2222@gmail.com>
2022-06-14 22:54:34 +0500

    paginator update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,11 +6,13 @@ import Categories from '../components/Categories';
 import PizzaCard from '../components/PizzaCard';
 import Sort from '../components/Sort';
 import Skeleton from '../components/PizzaCard/Skeleton';
+import Pagination from '../components/Pagination';
 
 function Home({ searchValue }) {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const [categoryId, setCatrgoryId] = React.useState(0);
+  const [currentPage, setCurrentPage] = React.useState(1);
   const [sortType, setSortType] = React.useState({ name: 'популярности', sortProperty: 'rating' });
 
   console.log(items);
@@ -20,29 +22,22 @@ function Home({ searchValue }) {
     const sortBy = sortType.sortProperty.replace('-', '');
     const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
     const category = categoryId > 0 ? `category=${categoryId}` : '';
+    const search = searchValue ? `&search=${searchValue}` : '';
 
     axios
       .get(
-        `https://62a72469bedc4ca6d7c31135.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`,
+        `https://62a72469bedc4ca6d7c31135.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
       )
       .then((res) => {
         setItems(res.data);
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue, currentPage]);
 
-  const pizzas = items
-    .filter((obj) => {
-      if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
-        return true;
-      }
-
-      return false;
-    })
-    .map(({ title, price, imageUrl, sizes, types }) => (
-      <PizzaCard title={title} price={price} image={imageUrl} sizes={sizes} types={types} />
-    ));
+  const pizzas = items.map(({ title, price, imageUrl, sizes, types }) => (
+    <PizzaCard title={title} price={price} image={imageUrl} sizes={sizes} types={types} />
+  ));
 
   const skeletons = [...new Array(10)].map((_, index) => <Skeleton key={index} />);
 
@@ -54,6 +49,7 @@ function Home({ searchValue }) {
       </div>
       <h2 className='content__title'>Все пиццы</h2>
       <div className='content__items'>{isLoading ? skeletons : pizzas}</div>
+      <Pagination onChangePage={(num) => setCurrentPage(num)} />
     </div>
   );
 }
